ee37868c915eae6466b85a509adb1dfafe110a5b
Solaman Huq <solaman@pocketgems.com>
2023-11-14 15:26:17 -0800

    CRC: test utility of method more closely

--- test/unit-test-example.js
+++ test/unit-test-example.js
@@ -1,6 +1,13 @@
 const { Example } = require('../example/feature')
 const { BaseTest, runTests } = require('../src/test')
 
+class CustomError extends Error {
+  constructor(message, customData) {
+    super(message)
+    this.customData = customData
+  }
+}
+
 class ExampleTest extends BaseTest {
   testMultiply () {
     expect(Example.multiply(2, 3)).toBe(6)
@@ -12,10 +19,18 @@ class ExampleTest extends BaseTest {
 
   async testCatchError () {
     const shouldThrow = () => {
-      throw new Error('hello!')
+      throw new CustomError('hello!', {
+        a: 33,
+        b: [1,2]
+      })
     }
     const error = await this.catchError(shouldThrow)
+    expect(error).toBeInstanceOf(CustomError)
     expect(error.message).toEqual('hello!')
+    expect(error.customData).toEqual({
+      a: 33,
+      b: [1,2]
+    })
     const doesNotThrow = () => 2
     await expect(this.catchError(doesNotThrow))
       .rejects.toThrow()
